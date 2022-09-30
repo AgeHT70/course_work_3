12010961ef7976fcde4563ec56e1659ff325831d
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-09-30 22:23:28 +0700

    add handler Error 404 and 500

--- run.py
+++ run.py
@@ -11,6 +11,17 @@ app = Flask(__name__)
 # Регистрируем блюпринты
 app.register_blueprint(posts_blueprint)
 
+
+@app.errorhandler(404)
+def not_found_page(e):
+    return "Page not found"
+
+
+@app.errorhandler(500)
+def server_error_page(e):
+    return "Server error"
+
+
 # Запускаем сервер, только если файл запущен, а не импортирован
 if __name__ == "__main__":
     app.run()
