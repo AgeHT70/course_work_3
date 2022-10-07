69653dfe46587b607dcaef337c57989608a215ab
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-10-07 22:24:37 +0700

    posts_dao: add method create_tag

--- app/posts/dao/posts_dao.py
+++ app/posts/dao/posts_dao.py
@@ -21,11 +21,25 @@ class PostsDAO:
                 return post
 
     def get_by_username(self, username):
-        posts = [post for post in self.load_data() if username.lower() == post["poster_name"].lower()]
+        posts = [post for post in self.load_data()
+                 if username.lower() == post["poster_name"].lower()]
         if not posts:
             raise ValueError("Нет такого пользователя")
         return posts
 
     def get_by_keyword(self, keyword):
-        posts = [post for post in self.load_data() if keyword.lower() in post["content"].lower()]
+        posts = [post for post in self.load_data()
+                 if keyword.lower() in post["content"].lower()]
         return posts
+
+    def create_tag(self, post_pk):
+        post = self.get_by_pk(post_pk)
+        content_list = post["content"].split()
+        for i in range(len(content_list)):
+            if content_list[i].startswith('#'):
+                word = content_list[i]
+                content_list[i] = word.replace(word,
+                                               f'<a href="/tag/{word[1:]}"'
+                                               f'>{word}</a>')
+        post["content"] = ' '.join(content_list)
+        return post
