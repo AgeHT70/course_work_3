802d759b65ffa9d01c842a7bfa51eee5e3022993
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-10-07 22:17:17 +0700

    run.py : add comments

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-from app import bookmarks
 # Импортируем блюпринты
 from app.posts.views import posts_blueprint
 from app.api.views import api_blueprint
@@ -17,6 +16,7 @@ app.register_blueprint(api_blueprint)
 app.register_blueprint(bookmarks_blueprint)
 
 
+#Обработчики ошибок 404 и 500
 @app.errorhandler(404)
 def not_found_page(e):
     return "Page not found"
@@ -30,3 +30,4 @@ def server_error_page(e):
 # Запускаем сервер, только если файл запущен, а не импортирован
 if __name__ == "__main__":
     app.run()
+
