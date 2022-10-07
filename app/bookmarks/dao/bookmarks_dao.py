c8a4488c318ca21b5bb4a3c28f827b440c84e23c
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-10-08 00:21:17 +0700

    bookmarks_dao.py add context and docstrings

--- app/bookmarks/dao/bookmarks_dao.py
+++ app/bookmarks/dao/bookmarks_dao.py
@@ -1,32 +1,48 @@
 import json
 
+from utils import load_data
+
 
 class BookmarksDAO:
 
     def __init__(self, path):
         self.path = path
 
-    def load_data(self):
-        with open(self.path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-        return data
-
-    def write_to_json(self, bookmarks):
+    def write_to_json(self, bookmarks: list[dict]) -> None:
+        """
+        Записывает данные в файл.
+        :param bookmarks:
+        :return:
+        """
         with open(self.path, "w", encoding="utf-8") as file:
             json.dump(bookmarks, file, ensure_ascii=False)
 
     def get_all(self):
-        bookmarks = self.load_data()
+        """
+        Загружает все bookmarks из файла.
+        :return: список bookmarks.
+        """
+        bookmarks = load_data(self.path)
         return bookmarks
 
-    def add(self, post):
+    def add(self, post: dict) -> None:
+        """
+        Записывает bookmark в файл.
+        :param post: словарь
+        :return: None
+        """
         bookmarks = self.get_all()
 
         if post not in bookmarks:
             bookmarks.append(post)
             self.write_to_json(bookmarks)
 
-    def del_bookmark(self, post_pk):
+    def del_bookmark(self, post_pk: int) -> None:
+        """
+        Удалаяет bookmark по номеру поста из файла.
+        :param post_pk: номер поста.
+        :return: None
+        """
         bookmarks = self.get_all()
         bookmarks = [bookmark for bookmark in bookmarks
                      if int(bookmark["pk"]) != post_pk]
