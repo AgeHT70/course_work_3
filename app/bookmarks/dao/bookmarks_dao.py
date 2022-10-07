41cacd3f7034b25a6ac422b6e1e389ea5a9ca5e9
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-10-07 22:26:33 +0700

    bookmarks_dao.py: add method write_to_json

--- app/bookmarks/dao/bookmarks_dao.py
+++ app/bookmarks/dao/bookmarks_dao.py
@@ -11,18 +11,26 @@ class BookmarksDAO:
             data = json.load(file)
         return data
 
+    def write_to_json(self, bookmarks):
+        with open(self.path, "w", encoding="utf-8") as file:
+            json.dump(bookmarks, file, ensure_ascii=False)
+
     def get_all(self):
         bookmarks = self.load_data()
         return bookmarks
 
     def add(self, post):
         bookmarks = self.get_all()
-        bookmarks.append(post)
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(bookmarks, file, ensure_ascii=False)
+
+        if post not in bookmarks:
+            bookmarks.append(post)
+            self.write_to_json(bookmarks)
 
     def del_bookmark(self, post_pk):
         bookmarks = self.get_all()
-        bookmarks = [bookmark for bookmark in bookmarks if int(bookmark["pk"]) != post_pk]
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(bookmarks, file, ensure_ascii=False)
+        bookmarks = [bookmark for bookmark in bookmarks
+                     if int(bookmark["pk"]) != post_pk]
+
+        self.write_to_json(bookmarks)
+
+
