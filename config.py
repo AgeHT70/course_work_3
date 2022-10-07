cf449bcbe03127f12236768bbfada3256c7da906
ageht <Vladimir.E.Pavlov@yandex.ru>
2022-10-08 00:19:14 +0700

    config.py create constants with os.path

---
+++ config.py
@@ -0,0 +1,5 @@
+from os import path
+
+BOOKMARKS_PATH = path.join('.', 'data', 'bookmarks.json')
+POSTS_PATH = path.join('.', 'data', 'posts.json')
+COMMENTS_PATH = path.join('.', 'data', 'comments.json')
